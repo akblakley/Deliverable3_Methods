9cc0cdb30177c5f5e3ce5af63fbd356bc8affc59
Amanda Blakley <51011635+blakleak@users.noreply.github.com>
2019-09-19 06:36:08 -0400

    ISM 4300 Deliverable 3 - Methods. Version 1.

--- Deliverable3_Methods/Program.cs
+++ Deliverable3_Methods/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ * Author: Amanda Blakley
+ * Date: 9/19/2019
+ * Description: This C# console application code demonstrates the use of
+ * methods after getting input from a user.
+*/
+
+using System;
 
 namespace Deliverable3_Methods
 {
@@ -6,7 +13,47 @@ namespace Deliverable3_Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to USF! May I ask you a few questions?");
+
+            try
+            {
+                // Declare variables 
+                int input_age;
+                string name;
+
+                // Prompt the user to enter their name 
+                Console.WriteLine("Please enter your name...");
+
+                // Allow the user to enter their name  
+                name = Console.ReadLine();
+
+                // Display a message saying "Hello" to the user
+                Console.WriteLine("Hello " + name);
+
+                // Prompt the user to enter their age 
+                Console.WriteLine("What is your age..?");
+                input_age = Convert.ToInt32(Console.ReadLine());
+
+                // This if statement test's to see if the input is between 18 and 100
+                if ((input_age > 17) && input_age <= 100))
+                {
+                    // Display a message stating the user's age
+                    Console.WriteLine("Your age is: " + input_age);
+                }
+                else
+                {
+                    Console.WriteLine("You must enter an age between 1 and 100");
+                    Console.WriteLine("Press any key to exit the program and try again...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                Console.ReadKey(true);
+            }
+            Console.WriteLine("Thank you!");
+            Console.WriteLine("Press any key to exit the program: ");
+            Console.ReadKey(true);
         }
     }
 }
